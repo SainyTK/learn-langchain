cb2b92221ec465dbe136c972f1eef48d2bddff5c
Tanakorn Karode <tanakorn.k@siametrics.com>
2024-06-30 22:00:09 +0700

    Rag tutorials complete

---
+++ 7_rag_app.py
@@ -0,0 +1,79 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+import bs4
+from langchain import hub
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+load_dotenv(override=True)
+
+llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/", ),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    )
+)
+docs = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+retriever = vectorstore.as_retriever()
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+def build_own_rag_chain():
+    prompt = hub.pull("rlm/rag-prompt")
+
+    rag_chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    result = rag_chain.invoke("What is Task Decomposition?")
+    print(result)
+
+def use_built_in_rag_chain():
+    system_prompt = (
+        "You are an assistant for quesiton-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you don't "
+        "know. Use three sentences maximum and keep the answer concise."
+        "\n\n"
+        "{context}"
+    )
+    
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            ("human", "{input}")
+        ]
+    )
+    
+    question_answer_chain = create_stuff_documents_chain(llm, prompt)
+    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+    
+    response = rag_chain.invoke({"input": "What is Task Decomposition?"})
+    print(response["answer"])
+    
+    for document in response["context"]:
+        print(document)
+
+
+# build_own_rag_chain()
+use_built_in_rag_chain()
+
+# cleanup
+vectorstore.delete_collection()
---
+++ 8_conversational_rag.py
@@ -0,0 +1,188 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+import bs4
+from langchain import hub
+from langchain.chains import create_retrieval_chain, create_history_aware_retriever
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.tools.retriever import create_retriever_tool
+from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.sqlite import SqliteSaver
+
+load_dotenv(override=True)
+
+# DB
+store = {}
+memory = SqliteSaver.from_conn_string(":memory:")
+
+llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    ),
+)
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+vectorstore = Chroma.from_documents(
+    documents=splits, embedding=OpenAIEmbeddings())
+retriever = vectorstore.as_retriever()
+
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
+
+contextualize_q_system_prompt = (
+    "Given a chat history and the latest user question "
+    "which might reference context in the chat history, "
+    "formulate a standalone question which can be understood "
+    "without the chat history. Do NOT answer the question, "
+    "just reformulate it if needed and otherwise return it as is."
+)
+
+def basic_rag_chain():
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            ("human", "{input}"),
+        ]
+    )
+
+    question_answer_chain = create_stuff_documents_chain(llm, prompt)
+    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+    response = rag_chain.invoke({"input": "What is Task Decomposition?"})
+    response["answer"]
+
+
+def history_aware_retriever_chain():
+    chat_history = []
+
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    history_aware_retriever = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
+
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}")
+        ]
+    )
+    
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+    
+    # Test
+    question = "What is Task Decomposition?"
+    ai_msg_1 = rag_chain.invoke({"input":question, "chat_history": chat_history})
+    chat_history.extend(
+        [
+            HumanMessage(content=question),
+            AIMessage(content=ai_msg_1["answer"])
+        ]
+    )
+    
+    second_question = "What are common ways of doing it?"
+    ai_msg_2 = rag_chain.invoke({"input": second_question, "chat_history": chat_history})
+    
+    print(ai_msg_2["answer"])
+    
+    
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+    
+def persisted_history_aware_chain(query: str) -> BaseChatMessageHistory:
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    history_aware_retriever = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
+
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}")
+        ]
+    )
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+    
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        output_messages_key="answer"
+    )
+    
+    config={
+        "configurable": {"session_id": "abb123"}
+    }
+    
+    response = conversational_rag_chain.invoke(
+        {"input": query},
+        config
+    )
+    
+    print(response["answer"])
+
+def history_aware_chain_with_agent(query: str):
+    tool = create_retriever_tool(
+        retriever,
+        "blog_post_retriever",
+        "Searches and returns excerpts from the Autonomouse Agents blog post."
+    )    
+    tools = [tool]
+    config = {"configurable": {"thread_id": "abc123"}}
+    
+    agent_executor = create_react_agent(llm, tools, checkpointer=memory)
+    
+    for s in agent_executor.stream(
+        {"messages": [HumanMessage(content=query)]}, config=config
+    ):
+        print(s)
+        print("----")
+    
+# history_aware_retriever_chain()
+
+# persisted_history_aware_chain("What is Task Decomposition?")
+# persisted_history_aware_chain("What are common ways of doing it?")
+
+history_aware_chain_with_agent("What is Task Decomposition")
+history_aware_chain_with_agent("What according to the blog post are common ways of doing it? redo the search")
